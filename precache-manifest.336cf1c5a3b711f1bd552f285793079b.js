dddbd69e98878a5f6e18224bee5dfe3b088cd127
gichan <rl0425@naver.com>
2020-08-25 19:13:26 +0900

    Updates

---
+++ precache-manifest.336cf1c5a3b711f1bd552f285793079b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89444fb6e465593c2a07203a373bdaf6",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "29b848e282b707ff8a95",
+    "url": "/movie_app_2020/static/css/main.aac7b001.chunk.css"
+  },
+  {
+    "revision": "11748d6dc8da0fe94e79",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29b848e282b707ff8a95",
+    "url": "/movie_app_2020/static/js/main.41155497.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
